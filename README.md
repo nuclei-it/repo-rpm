0a11ea9ee4494608636d36a348857f328829aac8
James Swineson <github@public.swineson.me>
2021-12-28 23:17:00 +0800

    update

---
+++ README.md
@@ -0,0 +1,17 @@
+# Nuclei System Technology Public RPM Repository
+
+## Usage
+
+## Development
+
+### Directory Structure
+
+### Environment Setup (Docker)
+
+```shell
+docker build -t repo-rpm:centos7 -f Dockerfile.centos7 .
+```
+
+### Compilation
+
+
--- SCRIPTED/slurm/build.sh
+++ SCRIPTED/slurm/build.sh
@@ -2,5 +2,10 @@
 
 VERSION="21.08.5"
 
-wget "https://download.schedmd.com/slurm/slurm-${VERSION}.tar.bz2"
-rpmbuild -ta "slurm-${VERSION}.tar.bz2" --with slurmrestd --with slurmsmwd --with hdf5 --with hwloc --with lua --with mysql --with numa --with ucx --with pmix --with nvml
+wget -c "https://download.schedmd.com/slurm/slurm-${VERSION}.tar.bz2"
+
+rpmbuild -ts "slurm-${VERSION}.tar.bz2"
+yum-builddep /root/rpmbuild/SRPMS/*.rpm
+yum -y install mysql-devel pkgconfig hwloc-devel mumactl-devel ucx-devel http-parser-devel json-c-devel lua-devel hdf5-devel lz4-devel
+rpmbuild -ta "slurm-${VERSION}.tar.bz2" --with slurmrestd --with slurmsmwd --with hdf5 --with hwloc --with lua --with mysql --with numa --with ucx
+
