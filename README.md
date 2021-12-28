5b7bb79661f68ee37efad2856152378b8718fcdf
James Swineson <github@public.swineson.me>
2021-12-28 23:55:53 +0800

    update slurm build script to remove yum-builddep

--- README.md
+++ README.md
@@ -9,9 +9,19 @@
 ### Environment Setup (Docker)
 
 ```shell
+pushd docker/
 docker build -t repo-rpm:centos7 -f Dockerfile.centos7 .
+popd
 ```
 
 ### Compilation
 
+```shell
+docker run --rm -it -v $(pwd):/appdata repo-rpm:centos7 bash
+bash /appdata/SCRIPTED/<software>/build.sh
+```
+
+Collect the artifacts from:
+- `/root/rpmbuild/RPMS/**/*.rpm`
+- `/root/rpmbuild/SRPMS/*.rpm`
 
--- SCRIPTED/slurm/build.sh
+++ SCRIPTED/slurm/build.sh
@@ -6,8 +6,9 @@ source scl_source enable devtoolset-10
 
 wget -c "https://download.schedmd.com/slurm/slurm-${VERSION}.tar.bz2"
 
-rpmbuild -ts "slurm-${VERSION}.tar.bz2"
-yum-builddep /root/rpmbuild/SRPMS/*.rpm
-yum -y install mysql-devel pkgconfig hwloc-devel mumactl-devel ucx-devel http-parser-devel json-c-devel lua-devel hdf5-devel lz4-devel
+#rpmbuild -ts "slurm-${VERSION}.tar.bz2"
+#yum-builddep /root/rpmbuild/SRPMS/*.rpm
+
+yum -y install munge-devel munge-libs pam-devel perl-ExtUtils-MakeMaker readline-devel mysql-devel pkgconfig hwloc-devel numactl-devel ucx-devel http-parser-devel json-c-devel lua-devel hdf5-devel lz4-devel
 rpmbuild -ta "slurm-${VERSION}.tar.bz2" --with slurmrestd --with slurmsmwd --with hdf5 --with hwloc --with lua --with mysql --with numa --with ucx
 
